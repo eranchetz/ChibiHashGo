95561fef5eb5c9c29e3a9a72264a63749ae844b8
Eran Chetz <eran@chetz.org>
2024-11-22 19:09:17 +0200

    add inital benchmarks

---
+++ chibihash_benchmark_test.go
@@ -0,0 +1,30 @@
+package chibihash
+
+import (
+	"testing"
+)
+
+func BenchmarkChibiHash64(b *testing.B) {
+	testCases := []struct {
+		name   string
+		key    []byte
+		length int
+		seed   uint64
+	}{
+		{"Empty Key", []byte(""), 0, 0},
+		{"Small Key", []byte("hi"), 2, 0},
+		{"Numeric Key", []byte("123"), 3, 0},
+		{"Alphabet Key", []byte("abcdefgh"), 8, 0},
+		{"Greeting Key", []byte("Hello, world!"), 13, 0},
+		{"Alphanumeric Key", []byte("qwertyuiopasdfghjklzxcvbnm123456"), 32, 0},
+		{"Long Alphanumeric Key", []byte("qwertyuiopasdfghjklzxcvbnm123456789"), 35, 0},
+	}
+
+	for _, tc := range testCases {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				ChibiHash64(tc.key, tc.length, tc.seed)
+			}
+		})
+	}
+}
